7dcde30ae87425c2d8796b10124d732a4771454c
Unknown <skyesladeut@outlook.com>
2019-08-28 00:28:28 -0600

    Working on Tutorial Scripts

--- Unity/Flight_Quest/Assets/Script/Incrementing_Bar_UI.cs
+++ Unity/Flight_Quest/Assets/Script/Incrementing_Bar_UI.cs
@@ -1,9 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class Incrementing_Bar_UI : MonoBehaviour
 {
-    public Image Hot_Bar, Warm_Bar, Cold_Bar, Rain_Bar, Snow_Bar;
+    private Image Bar;
+    public int Max_Val;
+    private float incVal;
+
+    private void Start()
+    {
+        Bar =  GetComponent<Image>();
+    }
+
+    public void SetVal(Float_Data val)
+    {
+        incVal = 1f / Max_Val;
+        Bar.fillAmount = val.value * incVal;
+    }
+
+    public void SetVal(float val)
+    {
+        incVal = 1f / Max_Val;
+        Bar.fillAmount = val * incVal;
+    }
 }
