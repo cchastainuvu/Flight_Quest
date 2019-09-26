71130e89942bad45543d7e7cc427900c17b3076a
cchastainuvu <10798368@my.uvu.edu>
2019-09-26 15:55:57 -0600

    VRTK mechanics, button & equip check

---
+++ Unity/Flight_Quest/Assets/Script/ToggleInteract.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Calysto Chastain
+public class ToggleInteract : MonoBehaviour
+{
+    public GameObject Obj;
+    private int _togCount;
+    public int _togLimit;
+
+    /*true = lights on;
+    false = lights off;*/
+
+    private void Start()
+    {
+        _togCount = 0;
+
+        Obj.SetActive(false);
+    }
+
+    private void Update()
+    {
+        ChangeState();
+    }
+
+    public void SetToggleCount()
+    {
+        _togCount++;
+    }
+
+    public void ChangeState()
+    {
+        if (_togCount < 0)
+            Obj.SetActive(false);
+
+        if (_togCount == _togLimit)
+        {
+            Obj.SetActive(true);
+        }
+
+        if (_togCount > _togLimit)
+        {
+            Obj.SetActive(false);
+            _togCount = 0;
+        }
+    }
+}
---
+++ Unity/Flight_Quest/Assets/Script/WaitForEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Calysto Chastain
+public class WaitForEvent : MonoBehaviour
+{
+    public int WaitTime;
+    public UnityEvent Event;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        Event.Invoke();
+    }
+}
